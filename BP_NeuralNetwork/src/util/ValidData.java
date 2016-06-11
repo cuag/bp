216a8e20f2de4da8f07275cbc4e3e619ed0b604e
vickey94 <vk@vickey.me>
2016-06-11 13:32:29 +0800

    update all

--- BP_NeuralNetwork/src/util/ValidData.java
+++ BP_NeuralNetwork/src/util/ValidData.java
@@ -17,6 +17,44 @@ public class ValidData {
 			}
 		}
 	
+	
+		
+		if(opCar.equals("N")){
+		
+			return false;
+		}
+		
+		if(TimeUtil.timeMinus(flight.getDepTime(),flight.getActDepTime())<-30){
+		
+			return false;
+		}
+	
+		
+		//按照1200KM/S，即20KM/s来算
+		int actFlyTime = Integer.parseInt(flight.getActFlyingTime());
+		int distKm = Integer.parseInt(flight.getDistKm());
+		if(actFlyTime ==0){
+			return false;
+		}
+		if(distKm/actFlyTime>20){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	public static boolean validData_SHA(Flight flight){
+		String comment = flight.getComment();
+		String opCar = flight.getOpCar();
+		String [] vali_comment ={"取消","未知","已備降","已排班","n/a","正在滑行","途中"};
+		for(String str : vali_comment){
+			if(str.equals(comment)){
+			
+				return false;
+			}
+		}
+	
 		if(!flight.getDepAirport().equals("SHA")){
 	
 			return false;
@@ -45,6 +83,8 @@ public class ValidData {
 		
 		return true;
 	}
+	
+	
 	
 	public static boolean validData(String comment){
 		
